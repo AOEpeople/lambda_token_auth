d19356ce3f288e23cf9b288acb05f3b7ef0040e1
Tolleiv Nietsch <github@tolleiv.de>
2021-04-19 10:49:46 +0200

    Adjust release/build process

--- build.sh
+++
@@ -1,4 +0,0 @@
-go mod tidy
-GOOS=linux CGO_ENABLED=0 GOARCH=amd64 go build -o dist/main cmd/main.go
-zip dist/function.zip dist/main
-cp dist/function.zip ../Team/terraform/stacks/000_token_auth/src
