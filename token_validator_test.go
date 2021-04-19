0e4e79ca2aa90611c37c7c36f37afe058e074777
Tolleiv Nietsch <github@tolleiv.de>
2021-04-19 08:52:29 +0200

    Cleanup tests

--- token_validator_test.go
+++ token_validator_test.go
@@ -7,14 +7,14 @@ import (
 )
 
 func TestMatchRuleToClaims(t *testing.T) {
-	tests := []struct {
+	tests := map[string]struct {
 		input *main.GitlabClaims
 		want  bool
 	}{
-		{input: &main.GitlabClaims{NamespaceId: "1"}, want: false},
-		{input: &main.GitlabClaims{NamespaceId: "2"}, want: true},
-		{input: &main.GitlabClaims{NamespaceId: "2", ProjectId: "5"}, want: true},
-		{input: &main.GitlabClaims{NamespaceId: "2", ProjectId: "4"}, want: false},
+		"simple_match": {input: &main.GitlabClaims{NamespaceId: "1"}, want: false},
+		"simple_mismatch": {input: &main.GitlabClaims{NamespaceId: "2"}, want: true},
+		"double_match": {input: &main.GitlabClaims{NamespaceId: "2", ProjectId: "5"}, want: true},
+		"double_mismatch": {input: &main.GitlabClaims{NamespaceId: "2", ProjectId: "4"}, want: false},
 	}
 	tokenClaims := &main.GitlabClaims{
 		NamespaceId: "2",
@@ -23,26 +23,29 @@ func TestMatchRuleToClaims(t *testing.T) {
 		ProjectPath: "/bla/blub",
 	}
 	tokenValidator := main.TokenValidator{}
-	for i, tc := range tests {
-		got := tokenValidator.MatchClaims(tokenClaims, tc.input)
-		if !reflect.DeepEqual(tc.want, got) {
-			t.Errorf("TestMatchRuleToClaims %d: expected got %v; want %v - inputClaims: %v", i, got, tc.want, tc.input)
-		}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := tokenValidator.MatchClaims(tokenClaims, tc.input)
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("expected got %v; want %v - inputClaims: %v", got, tc.want, tc.input)
+			}
+		})
 	}
 }
 
 func TestValidateClaimsForRule(t *testing.T) {
-	tests := []struct {
+	tests := map[string]struct {
 		inputRole string
 		inputNamespaceId string
 		wantRule    int
 		wantErr     bool
 	}{
-		{inputRole: "one", inputNamespaceId: "1",  wantRule: 0, wantErr: false},
-		{inputRole: "two", inputNamespaceId: "1", wantRule: 0, wantErr: true},
-		{inputRole: "three", inputNamespaceId: "1", wantRule: 0, wantErr: true},
-		{inputRole: "two", inputNamespaceId: "2", wantRule: 1, wantErr: false},
-		{inputRole: "two", inputNamespaceId: "1", wantRule: 0, wantErr: true},
+		"valid_check_one": {inputRole: "one", inputNamespaceId: "1",  wantRule: 0, wantErr: false},
+		"valid_check_two": {inputRole: "two", inputNamespaceId: "2", wantRule: 1, wantErr: false},
+		"claim_mismatch_one": {inputRole: "one", inputNamespaceId: "2", wantErr: true},
+		"claim_mismatch_two": {inputRole: "two", inputNamespaceId: "1", wantErr: true},
+		"role_missing": {inputRole: "three", inputNamespaceId: "1", wantErr: true},
+
 	}
 
 	var rules []main.Rule
@@ -55,15 +58,17 @@ func TestValidateClaimsForRule(t *testing.T) {
 		ClaimValues: main.GitlabClaims{NamespaceId: "2"},
 	})
 	tokenValidator := main.TokenValidator{}
-	for i, tc := range tests {
-		tokenClaims := &main.GitlabClaims{NamespaceId: tc.inputNamespaceId}
-		rule, err := tokenValidator.ValidateClaimsForRule(tokenClaims, tc.inputRole, rules)
-		if tc.wantErr && err == nil {
-			t.Errorf("TestValidateClaimsForRule %d: Expected empty rule within the result but got %s", i, rule.Role)
-		} else if !tc.wantErr && rule == nil {
-			t.Errorf("TestValidateClaimsForRule %d Expected rule %s but did not get it",i, tc.inputRole)
-		} else if !tc.wantErr && rules[tc.wantRule].Role != rule.Role {
-			t.Errorf("TestValidateClaimsForRule %d: Expected %s get %s", i, rules[tc.wantRule].Role, rule.Role)
-		}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			tokenClaims := &main.GitlabClaims{NamespaceId: tc.inputNamespaceId}
+			rule, err := tokenValidator.ValidateClaimsForRule(tokenClaims, tc.inputRole, rules)
+			if tc.wantErr && err == nil {
+				t.Errorf("expected empty rule within the result but got %s", rule.Role)
+			} else if !tc.wantErr && rule == nil {
+				t.Errorf("expected rule %s but did not get it", tc.inputRole)
+			} else if !tc.wantErr && rules[tc.wantRule].Role != rule.Role {
+				t.Errorf("expected %s get %s", rules[tc.wantRule].Role, rule.Role)
+			}
+		})
 	}
 }
